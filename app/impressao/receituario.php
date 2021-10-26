049a6b900435378caea04a0f6ba354be73fbb70a
Pedro Saddi <pedrosaddi@gmail.com>
2021-10-26 08:19:43 -0300

    visual

--- app/impressao/receituario.php
+++ app/impressao/receituario.php
@@ -3,42 +3,32 @@ include "print-header.php";
 ?>
 			
 <header class="titulo1">
-	<h1>Receituário</h1>
-	<p>01/01/2021</p>
+	<h1 style="margin:auto">Receituário Simples</h1>
 </header>
 
-<header class="titulo2">
-	<h1>Dados do Paciente</h1>
-</header>
-<div class="box">
+<div class="box box_empty">
 	<table>
 		<tr>
 			<td>
-				<h1>Nome</h1>
+				<h1>PACIENTE</h1>
 				<p>Kroner Machado Costa</p>
 			</td>
 			<td>
-				<h1>Idade</h1>
+				<h1>IDADE</h1>
 				<p>30 anos</p>
 			</td>
 			<td>
-				<h1>Sexo</h1>
+				<h1>SEXO</h1>
 				<p>Masculino</p>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="3">
-				<h1>Endereço</h1>
-				<p>Rua T-39, 875, Apto 1803, Setor Bueno. Goiânia-GO</p>
 			</td>
 		</tr>
 	</table>
 </div>
 
 <header class="titulo2">
-	<h1>Prescrição Medicamentosa</h1>
+	<h1>Prescrição</h1>
 </header>
-<div class="box">
+<div class="box box_empty">
 	<div class="prescricao">
 		<div class="prescricao__item">
 			<h1>01) Amoxilina 500mg</h1>
@@ -55,6 +45,53 @@ include "print-header.php";
 		</div>
 		<p class="prescricao__obs">Tomar 1 comprimido via oral a cada 8 horas durante 7 dias.</p>
 	</div>
+</div>
+
+<div class="box" style="margin-top:2.5rem;">
+	<table>
+		<tr>
+			<td colspan="2">
+				<h1>NOME DO MÉDICO</h1>
+				<p>KRONER MACHADO COSTA</p>
+			</td>
+			<td>
+				<h1>CRM</h1>
+				<p>284984874</p>
+			</td>
+			<td>
+				<h1>UF</h1>
+				<p>GO</p>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="3">
+				<h1>LOCAL DE ATENDIMENTO</h1>
+				<p>Rua 5, 691, The Prime Tamandaré Office, Térreo Loja 1, Setor Bueno</p>
+			</td>
+			<td>
+				<h1>CNES</h1>
+				<p>4787248</p>
+			</td>
+		</tr>
+		<tr>		
+			<td>
+				<h1>CIDADE</h1>
+				<p>GOIÂNIA</p>
+			</td>
+			<td>
+				<h1>UF</h1>
+				<p>GO</p>
+			</td>
+			<td>
+				<h1>TELEFONE</h1>
+				<p>(62) 3515-1717</p>
+			</td>
+			<td>
+				<h1>DATA DE EMISSÃO</h1>
+				<p>08/10/2021</p>
+			</td>
+		</tr>
+	</table>
 </div>
 
 <?php
