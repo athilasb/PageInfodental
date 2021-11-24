9b36d2cb9ec6b842be000d7acfd96d19584b22e1
André Teles <andre.telestp@gmail.com>
2021-11-24 20:53:47 -0300

    wip

--- app/pg_contatos_pacientes_evolucao_laboratorio.php
+++ app/pg_contatos_pacientes_evolucao_laboratorio.php
@@ -41,6 +41,82 @@
 		}
 	}
 
+    $laboratorios = [];
+    $sql->consult(
+    $_p."parametros_fornecedores",
+    "id,nome,tipo"," WHERE tipo ='LABORATORIO' AND lixo=0 ORDER BY nome ASC");
+    while($x=mysqli_fetch_object($sql->mysqry)) {
+        $laboratorios[$x->id]=$x;
+    }
+
+    $dentistas = [];
+    $sql->consult(
+        $_p."colaboradores",
+        "id,nome"," WHERE tipo_cro ='CD' AND lixo=0 ORDER BY nome ASC");
+    while($x=mysqli_fetch_object($sql->mysqry)) {
+        $dentistas[]=$x;
+    }
+
+    $servicosLaboratorios = [];
+    $idsLaboratorios = implode(',', array_keys($laboratorios));
+    $sql->consult("
+                {$_p}parametros_servicosdelaboratorio ips
+                    INNER JOIN
+                {$_p}parametros_servicosdelaboratorio_laboratorios ipsl
+                ON
+                  ips.id = ipsl.id_servicodelaboratorio
+                INNER JOIN
+                {$_p}parametros_fornecedores ipf
+                ON
+                   ipf.id = ipsl.id_fornecedor
+        ",
+        "ips.id, ips.titulo, ipsl.id_fornecedor"," WHERE ipf.id IN ({$idsLaboratorios}) AND ipf.lixo=0 ORDER BY ipf.nome ASC");
+    while($x=mysqli_fetch_object($sql->mysqry)) {
+        $x->titulo = utf8_encode($x->titulo);
+        $servicosLaboratorios[]=$x;
+    }
+
+    $listaDeServicos = [];
+    foreach ($servicosLaboratorios as $servico){
+        if(in_array($servico->id_fornecedor, array_keys($laboratorios))){
+            $listaDeServicos[$servico->id_fornecedor][] = $servico;
+        }
+    }
+
+    function objectToArray ($object) {
+    if(!is_object($object) && !is_array($object))
+        return $object;
+
+    return array_map('objectToArray', (array) $object);
+}
+//echo json_encode($listaDeServicos);
+//var_dump(json_last_error());
+//var_dump(json_last_error_msg());
+   // var_dump($listaDeServicos);die;
+//echo '<pre>';
+//var_dump( array_keys($laboratorios), $listaDeServicos);die;
+
+//SELECT
+//*
+//FROM
+//ident_parametros_servicosdelaboratorio ips
+//INNER JOIN
+//ident_parametros_servicosdelaboratorio_laboratorios ipsl
+//ON
+//ips.id = ipsl.id_servicodelaboratorio
+//INNER JOIN
+//ident_parametros_fornecedores ipf
+//ON
+//ipf.id = ipsl.id_fornecedor
+//WHERE
+//ipf.id IN (
+//    407
+//    -- SELECT id FROM ident_parametros_fornecedores a WHERE a.tipo ='LABORATORIO' AND a.lixo = 0
+//);
+
+//
+//    echo '<pre>';
+//    var_dump($destistas);die;
 
 	$campos=explode(",","nome,situacao,noem,sexo,foto,rg,rg_orgaoemissor,rg_estado,cpf,data_nascimento,profissao,estado_civil,telefone1,telefone1_whatsapp,telefone1_whatsapp_permissao,telefone2,email,instagram,instagram_naopossui,musica,indicacao_tipo,indicacao,cep,endereco,numero,complemento,bairro,estado,cidade,id_cidade,responsavel_possui,responsavel_nome,responsavel_sexo,responsavel_rg,responsavel_rg_orgaoemissor,responsavel_rg_estado,responsavel_datanascimento,responsavel_estadocivil,responsavel_cpf,responsavel_profissao,responsavel_grauparentesco,preferencia_contato");
 	
@@ -53,6 +129,95 @@
 		$values=$adm->values($campos,$cnt);
 		$values['data']=date('d/m/Y H:i',strtotime($cnt->data));
 	}
+
+    $values = [];
+    $values['titulo'] = 'Ordem de serviço X';
+//var_dump($_GET);die;
+//if(isset($_GET['form'])) {
+//
+//    $campos=explode(",","titulo");
+//
+//    foreach($campos as $v) $values[$v]='';
+//    $values['procedimentos']="[]";
+//    $values['pagamentos']="[]";
+//
+//    $sql->consult($_table,"id","where id_paciente=$paciente->id");
+//    $values['titulo']="Plano de tratamento ".($sql->rows+1);
+//
+//    $cnt='';
+//    if(isset($_GET['edita']) and is_numeric($_GET['edita'])) {
+//        $sql->consult($_table,"*","where id='".$_GET['edita']."' and lixo=0");
+//        if($sql->rows) {
+//            $cnt=mysqli_fetch_object($sql->mysqry);
+//            $values=$adm->values($campos,$cnt);
+//
+//            // Procedimentos
+//            $procedimentos=array();
+//            $where="where id_tratamento=$cnt->id and id_paciente=$paciente->id and id_unidade=$usrUnidade->id and lixo=0";
+//            $sql->consult($_table."_procedimentos","*",$where);
+//            while($x=mysqli_fetch_object($sql->mysqry)) {
+//
+//                $profissional=isset($_profissionais[$x->id_profissional])?$_profissionais[$x->id_profissional]:'';
+//                $iniciaisCor='';
+//                $iniciais='?';
+//                if(is_object($profissional)) {
+//                    $iniciais=$profissional->calendario_iniciais;
+//
+//                    $iniciaisCor=$profissional->calendario_cor;
+//                }
+//
+//                $valor=$x->valorSemDesconto;
+//                if($x->quantitativo==1) $valor*=$x->quantidade;
+//
+//                $procedimentos[]=array('id'=>$x->id,
+//                    'id_procedimento'=>(int)$x->id_procedimento,
+//                    'procedimento'=>utf8_encode($x->procedimento),
+//                    'id_profissional'=>(int)$x->id_profissional,
+//                    'profissional'=>utf8_encode($x->profissional),
+//                    'id_plano'=>(int)$x->id_plano,
+//                    'plano'=>utf8_encode($x->plano),
+//                    'quantitativo'=>(int)$x->quantitativo,
+//                    'quantidade'=>(int)$x->quantidade,
+//                    'id_opcao'=>(int)$x->id_opcao,
+//                    'opcao'=>utf8_encode($x->opcao),
+//                    'valorCorrigido'=>(float)$x->valor,
+//                    'valor'=>(float)$valor,
+//                    'desconto'=>(float)$x->desconto,
+//                    'obs'=>utf8_encode($x->obs),
+//                    'situacao'=>$x->situacao,
+//                    'iniciais'=>$iniciais,
+//                    'iniciaisCor'=>$iniciaisCor);
+//            }
+//            if($cnt->status=="APROVADO") {
+//                $values['procedimentos']=json_encode($procedimentos);
+//            } else {
+//                $values['procedimentos']=empty($cnt->procedimentos)?"[]":utf8_encode($cnt->procedimentos);
+//            }
+//
+//            // Pagamentos
+//            $pagamentos=array();
+//            $where="where id_tratamento=$cnt->id and id_paciente=$paciente->id and id_unidade=$usrUnidade->id and lixo=0";
+//            $sql->consult($_table."_pagamentos","*",$where);
+//            while($x=mysqli_fetch_object($sql->mysqry)) {
+//
+//                $pagamentos[]=array('id'=>$x->id,
+//                    'vencimento'=>date('d/m/Y',strtotime($x->data_vencimento)),
+//                    'valor'=>(float)$x->valor);
+//            }
+//
+//            if($cnt->status=="APROVADO") {
+//                $values['pagamentos']=json_encode($pagamentos);
+//            } else {
+//                $values['pagamentos']=empty($cnt->pagamentos)?"[]":utf8_encode($cnt->pagamentos);
+//            }
+//
+//            $values['pagamentos']=empty($cnt->pagamentos)?"[]":utf8_encode($cnt->pagamentos);
+//
+//        } else {
+//            $jsc->jAlert("Plano de Tratamento não encontrado!","erro","document.location.href='$_page?$url'");
+//            die();
+//        }
+//    }
 	?>
 	<section class="content">
 		
@@ -68,6 +233,20 @@
 					$(`.js-box`).hide();
 					$(`.js-box-${tipo}`).show();
 				})
+                const listServicos = JSON.parse('<?= json_encode($listaDeServicos) ?>');
+                const procedimentoSelect = $('select[name="procedimento"]');
+                const procedimentoOption = (value, label) => `<option value=${value}>${label}</option>`
+                const resetSelectOptions = (el) => {
+                    el.empty()
+                    el.append(procedimentoOption('', 'Selecione uma procedimento'))
+                }
+                $('select[name="laboratorio"]').on('change',function(){
+                    resetSelectOptions(procedimentoSelect)
+                    $(listServicos[this.value]).each(function(idx, item){
+                        procedimentoSelect.append(procedimentoOption(item.id, item.titulo))
+                        procedimentoSelect.val('').trigger('chosen:updated');
+                    })
+                })
 			});
 		</script>
 
@@ -88,17 +267,27 @@
 					</div>
 					<div class="filter-group">
 						<div class="filter-input">
-							<input type="text" name="" placeholder="título do serviço" />								
+							<input type="text" name="" placeholder="título do serviço" value="<?= $values['titulo'] ?>"/>
 						</div>
 					</div>
 					<div class="filter-group">
 						<div class="filter-input">
-							<select name=""><option value="">laboratório</option></select>
+							<select name="laboratorio">
+                                <option value="">laboratório</option>
+                                <?php foreach($laboratorios as $laboratorio): ?>
+                                    <option value="<?= $laboratorio->id ?>"> <?= utf8_encode($laboratorio->nome) ?> </option>
+                                <?php endforeach; ?>
+                            </select>
 						</div>
 					</div>
 					<div class="filter-group">
 						<div class="filter-input">
-							<select name=""><option value="">cirurgião dentista</option></select>
+							<select name="">
+                                <option value="">cirurgião dentista</option>
+                                <?php foreach($dentistas as $dentista): ?>
+                                    <option value="<?= $dentista->id ?>"> <?= utf8_encode($dentista->nome) ?> </option>
+                                <?php endforeach; ?>
+                            </select>
 						</div>
 					</div>
 					<div class="filter-group filter-group_right">
@@ -124,10 +313,8 @@
 							<dl>
 								<dt>Procedimento</dt>
 								<dd>
-									<select name="" class="chosen">
-										<option value="">Porcelana Injetada</option>
-										<option value="">Porcelana Fresada</option>
-										<option value="">Scan Service</option>
+									<select name="procedimento" class="chosen">
+										<option value="">Selecione um procedimento</option>
 									</select>
 								</dd>
 							</dl>
