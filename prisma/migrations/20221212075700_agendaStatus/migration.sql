5ca2cd3a97f40edf2abe0b079692fe484419ff71
Luciano <luciano@wlib.com.br>
2022-12-12 08:00:43 -0300

    migration

---
+++ prisma/migrations/20221212075700_agendaStatus/migration.sql
@@ -0,0 +1,21 @@
+INSERT INTO `ident_agenda_status` (`id`, `lixo`, `titulo`, `cor`, `kanban_ordem`) VALUES (8, '', 'RESERVA DE HORÁRIO', '#545559', '1');
+
+
+CREATE TABLE `ident_parametros_politicapagamento` (
+  `id` int NOT NULL,
+  `lixo` tinyint(1) NOT NULL,
+  `data` datetime NOT NULL,
+  `entrada` double NOT NULL COMMENT 'pagamento minimo (%)',
+  `parcelas` int NOT NULL COMMENT 'numero maximo de parcelas',
+  `de` double NOT NULL,
+  `ate` double NOT NULL,
+  `parcelasParametros` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `ident_parametros_politicapagamento`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `ident_parametros_politicapagamento`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
