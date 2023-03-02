384348fb47ccd0aa038109b9115e784686bd687f
Luciano <luciano@wlib.com.br>
2023-03-02 09:28:04 -0300

    assinatura

---
+++ app/assinaturas/anamnese.php
@@ -0,0 +1,122 @@
+<?php
+	$_assinaturaCliente=true;
+	include "print-header.php";
+	$evolucao = $paciente = $clinica = $solicitante = "";
+	$exames = $_exames = array();
+	if(isset($_GET['id'])) {
+		$sql->consult($_p."pacientes_evolucoes","*","where md5(id)='".addslashes($_GET['id'])."' and id_tipo=1");
+		if($sql->rows) {
+			$evolucao=mysqli_fetch_object($sql->mysqry);
+
+			$sql->consult($_p."colaboradores","id,nome","where id=$evolucao->id_usuario");
+			if($sql->rows) {
+				$solicitante=mysqli_fetch_object($sql->mysqry);
+			}
+
+			$sql->consult($_p."pacientes","*","where id=$evolucao->id_paciente");
+			if($sql->rows) {
+				$paciente=mysqli_fetch_object($sql->mysqry);
+			}
+
+			$_anamnesePerguntas=array();
+			$sql->consult($_p."pacientes_evolucoes_anamnese","*","where id_evolucao=$evolucao->id and lixo=0");
+			if($sql->rows) {
+				while($x=mysqli_fetch_object($sql->mysqry)) {
+					$_anamnesePerguntas[]=$x;
+				}
+			}
+			
+
+			
+		}
+	}
+
+
+	$jsc = new Js();
+
+	if(empty($evolucao)) {
+		$jsc->alert("Pedido de exame não cadastrado!","document.location.href='../dashboard.php'");
+		die();
+	}
+
+	if(empty($paciente)) {
+		$jsc->alert("Paciente não encontrado!","document.location.href='../dashboard.php'");
+		die();
+	}
+
+	if(empty($solicitante)) {
+		$jsc->alert("Solicitante não encontrado","document.location.href='../dashboard.php'");
+		die();
+	}
+
+	if($paciente->data_nascimento !="0000-00-00"){
+		$idade=idade($paciente->data_nascimento);	
+	}else{
+		$idade = "";
+	}
+
+
+
+?>
+			
+<header class="titulo1">
+	<h1>Ficha do Paciente</h1>
+	<p><?php echo date('d/m/Y',strtotime($evolucao->data));?></p>
+</header>
+
+<div class="ficha">
+	<table border="0">
+		<tr>
+			<td colspan="3"><strong><?php echo utf8_encode($paciente->nome);?></strong></td>
+		</tr>
+		<tr>
+			<td><?php echo $idade>1?"$idade anos":"$idade";?></td>
+			<td><?php echo $paciente->sexo=="M"?"Masculino":$paciente->sexo=="F"?"Feminino":'';?></td>
+			<td style="text-align:right;"><span class="iconify" data-icon="bxs:phone" data-inline="true"></span> <?php echo maskTelefone($paciente->telefone1);?></td>
+		</tr>
+	</table>
+</div>
+
+<header class="titulo2">
+	<span>
+		<h1>Formulário da Anamnese</h1>
+		<h2><?php echo utf8_encode($solicitante->nome);?></h2>
+	</span>
+</header>
+
+<div class="box">
+	<table>
+		<?php
+		foreach($_anamnesePerguntas as $p) {
+			$pergunta=json_decode($p->json_pergunta);
+		?>
+		<tr>
+			<td>
+				<p><strong><?php echo utf8_encode($p->pergunta);?></strong></p>
+				<p>
+					<?php 
+					if($pergunta->tipo=="simnao" or $pergunta->tipo=="simnaotexto") {
+						if($p->resposta=="SIM") echo "Sim";
+						else echo "Não";
+					} else if($pergunta->tipo=="nota") {
+						echo "Nota: ".$p->resposta;
+					} 
+					?>	
+				</p>
+				<?php
+				if(!empty($p->resposta_texto)) {
+					echo "<p>Resposta: ".utf8_encode($p->resposta_texto)."</p>";
+				}
+				?>
+			</td>
+		</tr>
+		<?php
+		}
+		?>
+		
+	</table>
+</div>
+
+<?php
+include "print-footer.php";
+?>
---
+++ app/assinaturas/print-header.php
@@ -0,0 +1,69 @@
+<?php
+	$dir="../";
+	require_once("../lib/conf.php");
+	require_once("../lib/classes.php");
+
+	$sql = new Mysql();
+
+	$sql->consult($_p."clinica","*","where id=1");
+	$unidade=mysqli_fetch_object($sql->mysqry);
+
+
+	$endereco = $imagem = '';
+
+	$endereco = utf8_encode($unidade->endereco);
+
+	$sql->consult($_p."clinica","*","");
+	if($sql->rows) {
+		$clinica=mysqli_fetch_object($sql->mysqry);
+		if(!empty($clinica->cn_logo)) {
+			$imagem=$_cloudinaryURL.'c_thumb,w_600/'.$clinica->cn_logo;
+		}
+	}
+	?>
+
+
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:og="http://ogp.me/ns#"
+      xmlns:fb="http://www.facebook.com/2008/fbml">
+
+<head>
+<meta charset="utf-8">
+<title>Infodental - Impressão</title>
+<link rel="stylesheet" type="text/css" href="../css/print.css" />
+<script defer src="https://code.iconify.design/1/1.0.3/iconify.min.js"></script>
+<script src="../js/jquery.js"></script>
+</head>
+
+<body>
+
+<div class="print-header" style="padding-top: 20px;">
+	<?php
+	if(!empty($imagem)) {
+	?>
+	<img src="<?php echo $imagem;?>" class="print-header__logo" style="width: auto;height: 30px;" />
+	<?php
+	} else {
+	?>
+	<img src="../img/logo-info.svg"  class="print-header__logo" style="width: auto;height: 25px;" />
+	<?php
+	}
+	?>
+</div>
+
+<div class="print-footer">
+	<p><span class="iconify" data-icon="bx:bxs-phone" data-inline="true"></span><span><?php echo maskTelefone($unidade->telefone);?></span><span class="iconify" data-icon="ri:whatsapp-fill" data-inline="true"></span><span><?php echo maskTelefone($unidade->whatsapp);?></span></p>
+	<p><?php echo $endereco;?></p>
+	<p>
+		<span><i class="iconify" data-icon="ph-globe-simple"></i> <a href="https://www.studiodental.dental"><?php echo $unidade->site;?></a></span>
+		<span><i class="iconify" data-icon="ph-instagram-logo"></i> <a href="https://instagram.com/studio.dental.dental"><?php echo $unidade->instagram;?></a></span>
+	</p>
+</div>
+
+<table class="print-table">
+	<thead><tr><td><div class="print-table-header">&nbsp;</div></td></tr></thead>
+	<tbody><tr><td>
+		<section class="print-content">
+
+			
