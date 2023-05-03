95fb7be178ef22f99d1291bf9cae04da827eab91
Luciano <luciano@wlib.com.br>
2023-05-03 11:20:54 -0300

    docker build 2.177

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,2 @@
-docker build ./ -t rg.fr-par.scw.cloud/infodental/app:2.176 && docker push rg.fr-par.scw.cloud/infodental/app:2.176
+docker build ./ -t rg.fr-par.scw.cloud/infodental/app:2.177 && docker push rg.fr-par.scw.cloud/infodental/app:2.177
 
