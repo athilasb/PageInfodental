1eacf9e585a22231cfe79ae97aacb42c0987033e
Rafael Oliveira <63177618+lRafaelOliveira@users.noreply.github.com>
2023-05-07 15:13:33 -0300

    add sql

---
+++ prisma/migrations/20230505120000_pagamentos_fluxo/migration.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `ident_financeiro_fluxo_pagamentos` (
+  `id` int NOT NULL,
+  `data_emissao` date NOT NULL,
+  `data_vencimento` date NOT NULL,
+  `tipo` enum('paciente','fornecedor','colaborador') NOT NULL,
+  `id_pagante` int NOT NULL,
+  `id_colaborador` int NOT NULL,
+  `valor` double NOT NULL,
+  `valor_multa` double NOT NULL DEFAULT '0',
+  `valor_taxa` double NOT NULL DEFAULT '0',
+  `valor_desconto` double NOT NULL DEFAULT '0',
+  `taxa_cartao` double NOT NULL DEFAULT '0',
+  `fusao` tinyint(1) NOT NULL,
+  `id_fusao` int NOT NULL,
+  `lixo` tinyint(1) NOT NULL,
+  `lixo_data` datetime NOT NULL,
+  `lixo_id_colaborador` int NOT NULL,
+  `id_formapagamento` int NOT NULL,
+  `id_operadora` int NOT NULL DEFAULT '0',
+  `id_bandeira` int NOT NULL DEFAULT '0',
+  `qtdParcelas` int NOT NULL,
+  `pago` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `ident_financeiro_fluxo_pagamentos`
+  ADD PRIMARY KEY (`id`);
+  
+ALTER TABLE `ident_financeiro_fluxo_pagamentos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=170;
+COMMIT;
