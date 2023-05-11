be3aa554494a0597138a6f2a1852fe41ce0778d7
athilasb <athila-sb@hotmail.com.br>
2023-05-11 14:41:30 -0300

    correção /pg_pacientes_resumo.php front

---
+++ app/externos/cadastros.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadastros pacientes</title>
+    <link rel="stylesheet" href="../css/cadastro.css">
+</head>
+<body>
+    <section>
+        <div class="page-cadastro colum-form">
+            <div>
+                <img src="../img/info-dental.png" alt="">
+            </div>
+            <div>
+                <div>Bem-vindo ao Studio Dental</div>
+                <div>Para sua segurança e para ter uma melhor experiência no seu tratamento, insira algumas informações importantes sobre você!  </div>
+                <a href="">Continuar</a>
+            </div>
+        </div>
+    </section>
+    
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+</body>
+</html>
