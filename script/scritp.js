99dd3b84779b2233d7282523cd95168509b372bc
athilasb <athila-sb@hotmail.com.br>
2023-05-30 09:31:43 -0300

    Muitas Altrações

--- script/scritp.js
+++ script/scritp.js
@@ -342,3 +342,35 @@ setTimeout(function() {
   $(".button-prices").click(function() {
     $(".modal-calendario").fadeIn();
   });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+          autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
